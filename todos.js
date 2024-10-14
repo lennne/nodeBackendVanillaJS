048938eac36c63b871aa5d101d894979ec6597d6
lennne <kobiansah@gmail.com>
2024-10-14 05:47:38 -0230

    server was created using vanillaJS by utilizing http core modules, and manually specifying header content and closing opened resources

---
+++ todos.js
@@ -0,0 +1,27 @@
+var todos = [
+    {
+        id:1,
+        title: "coding in Javascript",
+        description: "Working functions in javascript",
+        completed: false
+    },
+    {
+        id:2,
+        title: "Cooking Supper",
+        description: "Preparing rice and chicken",
+        completed: false
+    },
+    {
+        id:3,
+        title: "Taking a walk",
+        description: "Easy Time at the park",
+        completed: false
+    },
+    {
+        id:4,
+        title: "watching Netflix",
+        description: "Enjoying the new premiered series",
+        completed: false
+    }
+]
+module.exports = todos;
